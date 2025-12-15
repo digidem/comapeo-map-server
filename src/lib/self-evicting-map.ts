2fbc6bcd7aaa8645a7575f79751bcd29040d2952
Gregor MacLennan <gmaclennan@awana.digital>
2025-12-15 20:33:14 +0000

    fix uncaught error from SelfEvictingPromiseMap

--- src/lib/self-evicting-map.ts
+++ src/lib/self-evicting-map.ts
@@ -1,3 +1,5 @@
+import { noop } from './utils.js'
+
 /**
  * A Map that automatically evicts promise entries when they settle, and limits
  * the number of entries. Used to track active downloads.
@@ -8,11 +10,15 @@ export class SelfEvictingPromiseMap<K, V extends Promise<any>> extends Map<
 > {
 	override set(key: K, value: V): this {
 		super.set(key, value)
-		value.finally(() => {
-			if (this.get(key) === value) {
-				this.delete(key)
-			}
-		})
+		value
+			.finally(() => {
+				if (this.get(key) === value) {
+					this.delete(key)
+				}
+			})
+			// .finally() creates a new promise, so we need to catch errors here to
+			// avoid unhandled rejections
+			.catch(noop)
 		return this
 	}
 }
