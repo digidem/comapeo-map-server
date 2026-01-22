77cd16b70b86c24c559695493c37204c164a740c
Gregor MacLennan <gmaclennan@awana.digital>
2026-01-22 10:24:49 +0000

    update coverage output for coveralls

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 		environment: 'node',
 		coverage: {
 			provider: 'v8',
-			reporter: ['text', 'html', 'json'],
+			reporter: ['text', 'lcov'],
 			include: ['src/**/*.ts'],
 			exclude: ['src/**/*.test.ts', 'src/types.ts'],
 		},
